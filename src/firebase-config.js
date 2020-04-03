b1cd287b457036f5b24806ff062827880d4592e9
Paul <paulpalmieri@outlook.fr>
2020-04-03 13:24:07 -0400

    Add firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+export default {
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyAErzSTk3bVAwefp-cfjD5TuJa1DdXLhSY',
+    authDomain: 'kuizin-1c07f.firebaseapp.com',
+    databaseURL: 'https://kuizin-1c07f.firebaseio.com',
+    projectId: 'kuizin-1c07f',
+    storageBucket: 'kuizin-1c07f.appspot.com',
+    messagingSenderId: '432368744105',
+    appId: '1:432368744105:web:02d87ab50c24988cc91bb8',
+    measurementId: 'G-EVSVY65ESR',
+  },
+
+};
